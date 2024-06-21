4ebbd116d5dd0993814f434d5019c55d05f32a36
k2s09 <kavinsood15@gmail.com>
2024-06-21 11:28:33 +0530

    06/21/2024 11:28:32

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TodoCardComponent } from './components/todo-card/todo-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, TodoCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'todo-frontend';
-}
+export class AppComponent {}
